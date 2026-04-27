2db213b1c307927a3bd246210a39f10b67713df2
Alisson <145861427+AlissonPineda13@users.noreply.github.com>
2026-04-26 22:53:01 -0500

    correction on database conection

--- src/config.py
+++ src/config.py
@@ -5,6 +5,6 @@ class Config:
     # SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "mysql+pymysql://root:admin@localhost:3307/schema_valorant"
+        "mysql+pymysql://root:fFHojsfqbaBOwoPkgABElHDqgkFcLPiY@shuttle.proxy.rlwy.net:27918/schema_valorant"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
