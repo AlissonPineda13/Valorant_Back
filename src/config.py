55813beb4e2c1d043a35de408532ee38d7436c61
AlissonPineda13 <alissonpineda53@gmail.com>
2026-06-04 08:37:50 -0500

    enviroment corrections

--- src/config.py
+++ src/config.py
@@ -3,12 +3,7 @@ import os
 
 class Config:
     # SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-<<<<<<< HEAD
-        "mysql+pymysql://root:fFHojsfqbaBOwoPkgABElHDqgkFcLPiY@shuttle.proxy.rlwy.net:27918/schema_valorant"
-=======
-        "mysql+pymysql://root:admin@localhost:3306/schema_valorant"
->>>>>>> main
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    API_KEY = os.getenv("API_KEY")
+
